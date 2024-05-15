eb828d077d016df32e11e7c2f8f274909d77b100
Nico Saarela <127952345+Nico-sam@users.noreply.github.com>
2024-05-15 15:12:22 +0300

    Roskat roskiin ja pdf omannäköseksi

--- tcpdf/config/tcpdf_config.php
+++ tcpdf/config/tcpdf_config.php
@@ -109,12 +109,12 @@ define ('PDF_AUTHOR', 'TCPDF');
 /**
  * Header title.
  */
-define ('PDF_HEADER_TITLE', 'TCPDF Example');
+define ('PDF_HEADER_TITLE', 'Kiinteistöhuolto R.Autio');
 
 /**
  * Header description string.
  */
-define ('PDF_HEADER_STRING', "by Nicola Asuni - Tecnick.com\nwww.tcpdf.org");
+define ('PDF_HEADER_STRING', "by KoodariRyhmä 2024");
 
 /**
  * Document unit of measure [pt=point, mm=millimeter, cm=centimeter, in=inch].
